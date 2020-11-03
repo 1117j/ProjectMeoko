a28fd20a141ffc3f59955f6dc022e1ee228113ea
1117j <leehjin17@gmail.com>
2020-11-03 21:24:19 +0900

    project 먹코 upload
    2020.05 자바 프로젝트

---
+++ Meoko/src/meoko/Meoko.java
@@ -0,0 +1,19 @@
+package meoko;
+
+import meoko.view.ViewMenu;
+
+public class Meoko {
+	
+	ViewMenu	vm;
+	
+	
+	void Start() {		
+		vm = new ViewMenu();
+
+	}
+	
+	public static void main(String[] args) {
+
+	}
+
+}
